e23e583aef2d778a12525eb59f28d478770db6d2
aldibatyr <aldibatyrbekov@gmail.com>
2020-01-03 16:53:37 -0800

    deletedunused components

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,83 +5,6 @@ const Projects = () => {
   return (
     <Container as="section" className="Projects" id="projects">
       <Row className="Project-row">
-        <Col lg={7}>
-          <div className="Project">
-            <div className="Title">
-              <h1><b>VKUSS</b></h1>
-            </div>
-            <div className="divider">
-              <span className="line"></span>
-            </div>
-            <div className="description-wrapper">
-              <div className="description">
-                <p>VkusS determines your perfect hookah flavor based on 4 simple questions. Answer these questions and we are going to generate a flavor that would perfectly fit your liking!</p>
-              </div>
-            </div>
-            <div className="links-to-repos">
-              <div className="link-wrapper">
-                <div className="front-end">
-                  <a href="https://github.com/thinkful-ei-dragonfly/V2-capstone-aldiyar" rel="noopener noreferrer" target="_blank">
-                    <span>Front</span>
-                    <span>End</span>
-                    <span>Repo</span>
-                  </a>
-                </div>
-              </div>
-              <div className="divider-vertical"></div>
-              <div className="link-wrapper">
-                <div className="back-end">
-                  <a href="https://github.com/thinkful-ei-dragonfly/vkuss-server-aldiyar" rel="noopener noreferrer" target="_blank">
-                    <span>Back</span>
-                    <span>End</span>
-                    <span>Repo</span>
-                  </a>
-                </div>
-              </div>
-              <div className="divider-vertical"></div>
-              <div className="link-wrapper">
-                <div className="live-example">
-                  <a href="https://aldibatyrbekov-vkuss-app.now.sh" rel="noopener noreferrer" target="_blank">
-                    <span>Live</span>
-                    <span>Example</span>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </Col>
-        <Col lg={5}>
-          <div className="technologies-wrapper">
-            <div className="technologies-box">
-              <div className="front-end">
-                <div className="title">
-                  <h4>FRONT END</h4>
-                </div>
-                <ul>
-                  <li>HTML5</li>
-                  <li>CSS3</li>
-                  <li>JavaScript</li>
-                  <li>React</li>
-                  <li>Deployed on Zeit</li>
-                </ul>
-              </div>
-              <div className="back-end">
-                <div className="title">
-                  <h4>BACK END</h4>
-                </div>
-                <ul>
-                  <li>Node.js</li>
-                  <li>Express</li>
-                  <li>Knex</li>
-                  <li>PostgreSQL</li>
-                  <li>Deployed on Heroku</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </Col>
-      </Row>
-      <Row className="Project-row">
         <Col lg={5}>
           <div className="technologies-wrapper">
             <div className="technologies-box">
@@ -308,6 +231,83 @@ const Projects = () => {
                     <span>Example</span>
                   </a>
                 </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row className="Project-row">
+        <Col lg={7}>
+          <div className="Project">
+            <div className="Title">
+              <h1><b>VKUSS</b></h1>
+            </div>
+            <div className="divider">
+              <span className="line"></span>
+            </div>
+            <div className="description-wrapper">
+              <div className="description">
+                <p>VkusS determines your perfect hookah flavor based on 4 simple questions. Answer these questions and we are going to generate a flavor that would perfectly fit your liking!</p>
+              </div>
+            </div>
+            <div className="links-to-repos">
+              <div className="link-wrapper">
+                <div className="front-end">
+                  <a href="https://github.com/thinkful-ei-dragonfly/V2-capstone-aldiyar" rel="noopener noreferrer" target="_blank">
+                    <span>Front</span>
+                    <span>End</span>
+                    <span>Repo</span>
+                  </a>
+                </div>
+              </div>
+              <div className="divider-vertical"></div>
+              <div className="link-wrapper">
+                <div className="back-end">
+                  <a href="https://github.com/thinkful-ei-dragonfly/vkuss-server-aldiyar" rel="noopener noreferrer" target="_blank">
+                    <span>Back</span>
+                    <span>End</span>
+                    <span>Repo</span>
+                  </a>
+                </div>
+              </div>
+              <div className="divider-vertical"></div>
+              <div className="link-wrapper">
+                <div className="live-example">
+                  <a href="https://aldibatyrbekov-vkuss-app.now.sh" rel="noopener noreferrer" target="_blank">
+                    <span>Live</span>
+                    <span>Example</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col lg={5}>
+          <div className="technologies-wrapper">
+            <div className="technologies-box">
+              <div className="front-end">
+                <div className="title">
+                  <h4>FRONT END</h4>
+                </div>
+                <ul>
+                  <li>HTML5</li>
+                  <li>CSS3</li>
+                  <li>JavaScript</li>
+                  <li>React</li>
+                  <li>Deployed on Zeit</li>
+                </ul>
+              </div>
+              <div className="back-end">
+                <div className="title">
+                  <h4>BACK END</h4>
+                </div>
+                <ul>
+                  <li>Node.js</li>
+                  <li>Express</li>
+                  <li>Knex</li>
+                  <li>PostgreSQL</li>
+                  <li>Deployed on Heroku</li>
+                </ul>
               </div>
             </div>
           </div>
